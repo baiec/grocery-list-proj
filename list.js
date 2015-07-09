d9dc8fc1adeaf5ee127123ce55127932b3e94570
UHH <student10@js-c02mxkjhg083.punahou.edu>
2015-07-09 13:23:01 -1000

    made it so when you click on an item you type it dupes it

--- list.js
+++ list.js
@@ -1,16 +1,19 @@
-var array = [];
-
 //This function takes the textarea value, puts it in an array,
-//clears the textarea, then adds a new <p> and puts the textarea value on the page as clickable text.
+//clears the textarea, then adds a new <p>,
+//and puts the textarea value on the page as clickable text.
 $("#submit").click(function(){
 	var item = $("#inputList").val();
-	array.push(item);
 	$("#inputList").val("");
-	$("#list").append("<p id=''></p>");
-	$("#buttons").append(item + "<br>");
+	$("#list").append("<li class='hi'>"+item+"</li>");
 });
 
-function thing(item){
+$(document).on('click','li',function(){
+		$("#toBuy").append($(this).clone().appendTo("#buy"));
+});
+	
 
-}
+
+
+
+
 
